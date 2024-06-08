77de0e0b65128496a5c675659861afd02ae4014a
xxxmin0224 lee <imin0224@gmail.com>
2024-06-08 05:14:18 +0000

    수정

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 let slideIndex = 0;
-let slides = document.getElementsByClassName("slides");
-
 showSlides();
 
 function showSlides() {
+    let slides = document.getElementsByClassName("slides");
     for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('active');
+        slides[i].style.opacity = 0;  
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].classList.add('active');
+    slides[slideIndex-1].style.opacity = 1;
     setTimeout(showSlides, 3000); // 3초마다 슬라이드 변경
 }
 
@@ -17,7 +16,6 @@ function plusSlides(n) {
     slideIndex += n - 1;
     showSlides();
 }
-
 
 function showHoverImage(event, imageUrl) {
     const hoverImage = document.getElementById('hover-image');
